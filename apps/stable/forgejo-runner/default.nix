4bcd28a22506a4f38c09610ac31c298a1ff9741e
Nathan Kidd <nathankidd@hey.com>
2026-04-11 20:15:04 +0100

    .

--- apps/stable/forgejo-runner/default.nix
+++ apps/stable/forgejo-runner/default.nix
@@ -142,7 +142,7 @@ in
 
     runnerImage = mkOption {
       type = types.str;
-      default = "code.forgejo.org/forgejo/runner:15.0";
+      default = "code.forgejo.org/forgejo/runner:14.0";
       description = "Forgejo runner container image.";
     };
 
